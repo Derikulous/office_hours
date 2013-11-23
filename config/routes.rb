17cf5bfb1dd31f81654b5878d8980985b38af40b
Derik Strattan <derik.strattan@gmail.com>
2013-11-23 10:39:21 -0800

    added Devise users. root at home#index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 OfficeHours::Application.routes.draw do
+  devise_for :users
   resources :solutions
 
   resources :questions
@@ -6,6 +7,7 @@ OfficeHours::Application.routes.draw do
   resources :exams
 
   resources :answers
+  root :to => "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -47,7 +49,7 @@ OfficeHours::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
